761ae7536db939235debdbebe96f6a2d5069723a
dongnycklee <89719426+dongnycklee@users.noreply.github.com>
2022-02-10 11:24:03 +0900

    매입자정보 리셋

--- src/js/app.js
+++ src/js/app.js
@@ -37,6 +37,7 @@ App = {
 		$.getJSON('RealEstate.json', function(data) {
       App.contracts.RealEstate = TruffleContract(data);
       App.contracts.RealEstate.setProvider(App.web3Provider);
+      return App.loadRealEstates();
     });
   },
 
@@ -59,6 +60,7 @@ App = {
         $('#name').val('');
         $('#age').val('');
         $('#buyModal').modal('hide');
+        return App.loadRealEstates();
       }).catch(function(err) {
         console.log(err.message);
       });
@@ -66,7 +68,32 @@ App = {
   },
 
   loadRealEstates: function() {
-	
+    App.contracts.RealEstate.deployed().then(function(instance) {
+      return instance.getAllBuyers.call();
+    }).then(function(buyers) {
+      for (i = 0; i < buyers.length; i++) {
+        if (buyers[i] !== '0x0000000000000000000000000000000000000000') {
+          var imgType = $('.panel-realEstate').eq(i).find('img').attr('src').substr(7);
+
+          switch(imgType) {
+            case 'apartment.jpg':
+            $('.panel-realEstate').eq(i).find('img').attr('src', 'images/apartment_sold.jpg')
+            break;
+            case 'townhouse.jpg':
+            $('.panel-realEstate').eq(i).find('img').attr('src', 'images/townhouse_sold.jpg')
+            break;
+            case 'house.jpg':
+            $('.panel-realEstate').eq(i).find('img').attr('src', 'images/house_sold.jpg')
+            break;
+          }
+
+          $('.panel-realEstate').eq(i).find('.btn-buy').text('매각').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-buyerInfo').removeAttr('style');
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    })
   },
 	
   listenToEvents: function() {
